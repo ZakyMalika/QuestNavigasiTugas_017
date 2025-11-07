74fab40717f97b3488520eea1ed2da426870b08e
ZakyMalika <zakymalika0987@gmail.com>
2025-11-08 01:03:33 +0700

    membuat text field

--- app/src/main/java/com/example/tugasprak_6/View/Pendaftaran.kt
+++ app/src/main/java/com/example/tugasprak_6/View/Pendaftaran.kt
@@ -1,22 +1,43 @@
 package com.example.tugasprak_6.View
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tugasprak_6.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Pendaftaran(modifier: Modifier){
+
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    var Nama by remember { mutableStateOf("") }
+    var Alamat by remember { mutableStateOf("") }
+    var JK by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -27,6 +48,51 @@ fun Pendaftaran(modifier: Modifier){
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Text(text = stringResource(id = R.string.judul_formulir))
+        Text(
+            text = stringResource(id = R.string.nama_lengkap),
+            fontSize = 35.sp,
+            color = Color.Black
+        )
+
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .width(350.dp)
+                .padding(top = 30.dp, start = 30.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = { textNama = it }
+        )
+
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+                        onClick = { textJK = item }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(selected = textJK == item, onClick = { textJK = item })
+                    Text(item)
+                }
+            }
+        }
+
+//        ini buat dropdown
+
+//        nih baru alamat
+
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .width(350.dp)
+                .padding(top = 30.dp, start = 30.dp),
+            label = { Text(text = "Alamat") },
+            onValueChange = { textAlamat = it }
+        )
     }
 }
