ea4b16f8bc7f7722ad2c1ae64c65a0664120c6b4
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 19:57:10 +0700

    membuat file dashboard

---
+++ app/src/main/java/com/example/tugasprak_6/View/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.tugasprak_6.View
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun Dashboard(modifier: Modifier){
+    Column {
+
+    }
+}
