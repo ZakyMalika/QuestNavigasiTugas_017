eedbda2ca2688ce920ce53700f76611e2db1924d
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 20:36:18 +0700

    membuat Text untuk judul tampilan awal

--- app/src/main/java/com/example/tugasprak_6/MainActivity.kt
+++ app/src/main/java/com/example/tugasprak_6/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tugasprak_6.View.Dashboard
 import com.example.tugasprak_6.ui.theme.Tugasprak_6Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugasprak_6Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Dashboard(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/tugasprak_6/View/Dashboard.kt
+++ app/src/main/java/com/example/tugasprak_6/View/Dashboard.kt
@@ -1,12 +1,38 @@
 package com.example.tugasprak_6.View
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasprak_6.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Dashboard(modifier: Modifier){
-    Column {
+fun Dashboard(
+//    onMasukBtnClick:()-> Unit,
+    modifier: Modifier
+){
+  Column(modifier = Modifier
+      .padding(top = 200.dp, start = 110.dp),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.Center)
+  {
+    Text(text = stringResource(R.string.welcome),
+        fontSize = 40.sp)
+  }
 
-    }
+
 }
+
