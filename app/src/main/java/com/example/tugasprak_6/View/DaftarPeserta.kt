90c8d87fa298668ecc90b8c96f2b58f50559a62f
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 22:21:13 +0700

    membuat file untuk tampilan Daftar Peserta

--- app/src/main/java/com/example/tugasprak_6/View/DaftarPeserta.kt
+++ app/src/main/java/com/example/tugasprak_6/View/DaftarPeserta.kt
@@ -1,8 +1,18 @@
 package com.example.tugasprak_6.View
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasprak_6.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -10,5 +20,12 @@ import androidx.compose.ui.Modifier
 fun DaftarPeserta(
     modifier: Modifier
 ){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = stringResource(id = R.string.list))
 
+
+    }
 }
